64124589dd3765e582ff03eda295d7eecf5b2512
Oleg Glumov <darek2587@gmail.com>
2022-05-06 12:32:51 +0300

    add task Kamen', nozhnichj, bumaga

---
+++ KNB.py
@@ -0,0 +1,47 @@
+# Камень-ножницы-бумага (вариант игрок против игрока и игрок против бота)
+
+import random
+
+# Игрок против бота
+
+list = ("Камень", "Ножницы", "Бумага")
+slovo = input(f'Введите один из вариантов {list}: ')
+
+player = slovo
+print(f'Игрок: {player}')
+bot = random.choice(list)
+print(f'Бот: {bot}')
+
+if (player == 'Ножницы' and bot == 'Камень') or \
+    (player == 'Бумага' and bot == 'Ножницы') or \
+    (player == 'Камень' and bot == 'Бумага'):
+    print('Игрок проиграл')
+    
+elif player == bot:
+    print('Ничья')
+
+else:
+    print('Игрок победил')
+    
+
+exit()
+# Игрок против игрока
+
+list = ("Камень", "Ножницы", "Бумага")
+slovo = input(f'Первый игрок - введите один из вариантов {list}: ')
+slovo2 = input(f'Второй игрок - введите один из вариантов {list}: ')
+
+player = slovo
+print(f'Первый игрок: {player}\n')
+player2 = slovo2
+print(f'Второй игрок: {player2}\n')
+
+if (player == 'Ножницы' and player2 == 'Камень') or \
+    (player == 'Бумага' and player2 == 'Ножницы') or \
+    (player == 'Камень' and player2 == 'Бумага'):
+    print('Победил второй игрок')
+
+elif player == player2:
+    print('Ничья')
+else:
+    print('Победил первый игрок')
