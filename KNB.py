ddb30e7527b299b30158ea24aa89b9bfd63512eb
Oleg Glumov <darek2587@gmail.com>
2022-05-07 23:05:17 +0300

    KMN v. 1.5

--- KNB.py
+++ KNB.py
@@ -3,33 +3,67 @@
 import random
 
 # Игрок против бота
+list = 'камень', 'ножницы', 'бумага'
+p = int(input('Введите количество попыток: '))
+i = 1
+count_player = 0
+count_bot = 0
 
-while True:
+while i <= p:
 
-    list = ("Камень", "Ножницы", "Бумага")
-    slovo = input(f'Введите один из вариантов {list}: ')
+    slovo = input(f'Введите один из вариантов ({list}): ')
+    player = slovo.lower()
+    
+    if player not in list:
+        print('Не верный вариант')
+        break
 
-    player = slovo
     print(f'Игрок: {player}')
-    bot = random.choice(list)
-    print(f'Бот: {bot}')
+    # for i in range(100):
+    if i >= 2:
+        if i-1:
+            player == 'камень'
+            bot = 'бумага'
+        elif player == 'ножницы':
+            bot = 'камень'
+        elif player == 'бумага':
+            bot = 'ножницы'
+        print(f'Бот: {bot}')
+    else:
+        bot = random.choice(list).lower()
+        print(f'Бот: {bot}')
 
-    if (player == 'Ножницы' and bot == 'Камень') or \
-        (player == 'Бумага' and bot == 'Ножницы') or \
-        (player == 'Камень' and bot == 'Бумага'):
-        print('Игрок проиграл')
-        
+
+    if (player == 'ножницы' and bot == 'камень') or \
+        (player == 'бумага' and bot == 'ножницы') or \
+        (player == 'камень' and bot == 'бумага'):
+        print('\nИгрок проиграл')
+        count_bot += 1
+            
     elif player == bot:
-        print('Ничья')
+        print('\nНичья')
 
     else:
         print('\nИгрок победил')
-    
+        count_player += 1
+        
     play_again = ''
-    play_again = input('\nСыграем ещё? (да/нет):\n')
+    play_again = input('\nСыграем ещё? (да/нет):')
     if play_again != 'да':
         break
-
+    
+    i += 1
+    
+if i > p:
+    print('Попытки исчерпаны {i} из {p}')
+    print(f'Побед игрока: {count_player}')
+    print(f'Побед бота: {count_bot}')
+    
+if play_again == 'нет':
+    print(f'Пройдено {i} попыток из {p}')
+    print(f'Побед игрока: {count_player}')
+    print(f'Побед бота: {count_bot}')
+    
 exit()
 # Игрок против игрока
 
